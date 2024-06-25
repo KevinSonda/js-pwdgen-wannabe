43a3602d5345c2aad3130de61bd0afc18d05f262
KevinSonda <kevinsonda@gmail.com>
2024-06-25 17:33:32 +0200

    [UPD]Aggiunta prompt per l'inserimento del cognome

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@
 let first_name = prompt("Inserisci il tuo nome");
 console.log(first_name)
 
+//Permesso di inserire il cognome all'utente
+let second_name = prompt("Inserisci il tuo cognome");
+console.log(second_name)
