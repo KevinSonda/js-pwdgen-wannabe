79e7f902c20e49ce9b72bc730ea280067eafcbe6
KevinSonda <kevinsonda@gmail.com>
2024-06-25 17:51:08 +0200

    [UPD]Creazione stringa

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@ let first_name = prompt("Inserisci il tuo nome");
 console.log(first_name);
 
 //Permesso di inserire il cognome all'utente
-let second_name = prompt("Inserisci il tuo cognome");
-console.log(second_name);
+let last_name = prompt("Inserisci il tuo cognome");
+console.log(last_name);
 
 //Permesso di inserire il colore all'utente
 let color = prompt("Inserisci il tuo colore preferito");
 console.log(color);
+
+//Creo una variabile dove assegno i valori inseriti precedentemente con il  valore 23
+let string = `${first_name}${last_name}${color}23`;
+console.log(string);
