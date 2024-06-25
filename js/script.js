bf6074b90174e06346e16d44e49518e41db1aa70
KevinSonda <kevinsonda@gmail.com>
2024-06-25 17:38:07 +0200

    [UPD]Aggiunta prompt che permette di inserire il colore all'utente

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 //Permesso di inserire il nome all'utente
 let first_name = prompt("Inserisci il tuo nome");
-console.log(first_name)
+console.log(first_name);
 
 //Permesso di inserire il cognome all'utente
 let second_name = prompt("Inserisci il tuo cognome");
-console.log(second_name)
+console.log(second_name);
+
+//Permesso di inserire il colore all'utente
+let color = prompt("Inserisci il tuo colore preferito");
+console.log(color);
